9811a3ecf64ba49ea50dd573f88037775dd26c41
Jamesson Seren de Arruda <jamesson.arruda@sga.pucminas.br>
2023-07-28 09:13:26 -0400

    added revenue average analysis in report.csv

--- src/services/report/GenerateReportService.ts
+++ src/services/report/GenerateReportService.ts
@@ -8,7 +8,7 @@ class GenerateReportService {
 
     const combinedData = await this.combineData()
 
-    const csvData = parse(combinedData, { fields: ['dispenser_id', 'dispenser_name', 'revenue', 'time_running_in_minutes', 'operations'] });
+    const csvData = parse(combinedData, { fields: ['dispenser_id', 'dispenser_name', 'revenue','revenue_avg_analysis', 'time_running_in_minutes', 'operations'] });
 
     const path = "./exports/report.csv";
 
@@ -18,6 +18,12 @@ class GenerateReportService {
   private async combineData() {
     const dispensersData = await this.getDispensersData();
     const servicesData = await this.getServicesData();
+
+    const totalRevenue = dispensersData.reduce((total, dispenser) => {
+      return total + dispenser.total_amount
+    }, 0)
+
+    const revenue_avg = totalRevenue / dispensersData.length
 
     const combinedData = [];
 
@@ -29,6 +35,16 @@ class GenerateReportService {
       const servicesForDispenser = servicesData.filter(
         (service) => service.dispenser_id === dispenser_id
       );
+
+      let revenueAvgAnalysis: string;
+      if (revenue < revenue_avg) {
+        revenueAvgAnalysis = 'below_average';
+      } else if (revenue > revenue_avg) {
+        revenueAvgAnalysis = 'above_average';
+      } else {
+        revenueAvgAnalysis = 'average';
+      }
+
 
       const totalTimeRunning = servicesForDispenser.reduce(
         (totalTime, service) => {
@@ -47,6 +63,7 @@ class GenerateReportService {
         dispenser_id: dispenser_id,
         dispenser_name: dispenser_name,
         revenue: revenue.toFixed(2),
+        revenue_avg_analysis: revenueAvgAnalysis,
         time_running_in_minutes: totalTimeRunning.toFixed(2),
         operations: totalOperations,
       };
@@ -57,6 +74,7 @@ class GenerateReportService {
     return combinedData
   }
 
+ 
   private async getDispensersData() {
     return await prismaClient.dispensers.findMany();
   }
